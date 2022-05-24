1405ec62f9c13eb82d996224c8efc93ea0d72aff
rajis31 <raj350@mail.fresnostate.edu>
2022-05-24 07:05:36 -0700

    initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="shadow-md w-[400px] h-[400px] flex flex-col  justify-center content-center">
+     <p
+    class="
+        text-6xl 
+        font-bold
+        px-4 
+        py-4 
+        text-purple-500
+        h-[100px]
+        "
+  >
+    Hello world!
+  </p>
+</div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
